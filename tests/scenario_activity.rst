4fc0f19b4c1fa90f6749213eb1fc09d65c76a8a3
Mathias Behrle <mathiasb@m9s.biz>
2024-02-11 21:00:49 +0100

    Use our template for a working setup.py.

---
+++ tests/scenario_activity.rst
@@ -0,0 +1,12 @@
+=================
+Activity Scenario
+=================
+
+Imports::
+
+    >>> from proteus import Model, Wizard
+    >>> from trytond.tests.tools import activate_modules
+
+Activate modules::
+
+    >>> config = activate_modules('activity')
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,13 +1,11 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
-
-from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ActivityTestCase(CompanyTestMixin, ModuleTestCase):
-    'Test Activity module'
+class ActivityTestCase(ModuleTestCase):
+    "Test Activity module"
     module = 'activity'
 
 
