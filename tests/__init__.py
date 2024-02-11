48e819b68f568f57a71a394e936ad5067052912f
Mathias Behrle <mathiasb@m9s.biz>
2024-02-11 21:00:49 +0100

    Use our template for a working setup.py.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,3 @@
-# This file is part of activity module for Tryton. The COPYRIGHT file at
-# the top level of this repository contains the full copyright notices and
-# license terms.
-try:
-    from trytond.modules.activity.tests.test_activity import suite
-except ImportError:
-    from .test_activity import suite
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
-__all__ = ['suite']
---
+++ tests/test_module.py
@@ -0,0 +1,12 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class ActivityTestCase(ModuleTestCase):
+    "Test Activity module"
+    module = 'activity'
+
+
+del ModuleTestCase
