2695442a31f7c3b557aeed1fc00effe86a2d9e13
Albert Cervera i Areny <albert@nan-tic.com>
2024-02-16 16:14:57 +0100

    Do not copy activities on party.party copy().

--- party.py
+++ party.py
@@ -3,12 +3,18 @@
 from trytond.model import fields
 from trytond.pool import PoolMeta
 
-__all__ = ['Party', 'PartyReplace']
-
 
 class Party(metaclass=PoolMeta):
     __name__ = "party.party"
     activities = fields.One2Many('activity.activity', 'party', 'Activities')
+
+    @classmethod
+    def copy(cls, parties, default=None):
+        if default is None:
+            default = {}
+        default = default.copy()
+        default.setdefault('activities', None)
+        return super(Party, cls).copy(parties, default=default)
 
 
 class PartyReplace(metaclass=PoolMeta):
